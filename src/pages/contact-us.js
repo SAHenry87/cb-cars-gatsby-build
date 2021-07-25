a20c53773099843fde99a23f657b27d60424ec57
Stephen Henry <stephenahenry87@gmail.com>
2021-07-26 00:15:09 +0100

    Uploading

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -6,7 +6,7 @@ import Seo from "../components/seo"
 const ContactPage = () => (
   <Layout>
     <Seo title="Contact us" />
-    <section id="contact-page">
+    <section id="contact-page"> 
 
                 <div className="lines">
                     <div className="line"></div>
